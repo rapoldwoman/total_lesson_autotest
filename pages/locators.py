898c5cb76dab2c78be7be35ec7f9494a81988bb2
Anton Pogorelov <a.pogorelov@biglion.ru>
2024-12-15 10:48:52 +0300

    добавил локаторы на логинпейдж и проверки

--- pages/locators.py
+++ pages/locators.py
@@ -4,3 +4,15 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    LOGIN_INPUT_EMAIL = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_INPUT_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    BUTTON_LOGIN = (By.CSS_SELECTOR, '[value="Log In"]')
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTER_INPUT_EMAIL = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTER_INPUT_PASSWORD1 = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTER_INPUT_PASSWORD2 = (By.CSS_SELECTOR, "#id_registration-password2")
+    BUTTON_REGISTER = (By.CSS_SELECTOR, '[value="Register"]')
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert "/login" in self.open_url(), "Видимо неверный урл"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.browser.find_element(*LoginPageLocators.LOGIN_FORM), "Логин формы нет"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.browser.find_element(*LoginPageLocators.REGISTER_FORM), "Регистр формы нет"
