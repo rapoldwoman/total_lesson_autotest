651f4432cae25a80bf880e98aadb574abd25c3bb
Anton Pogorelov <a.pogorelov@biglion.ru>
2024-12-20 11:11:00 +0300

    добавил метод проверки отсутствия элемента и  исчезания

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,10 @@
 from selenium.common.exceptions import NoAlertPresentException
 import math
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+
+
 
 class BasePage:
     def __init__(self, browser, url):
@@ -22,3 +27,19 @@ class BasePage:
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
